a8a81cb9cab9f938dc62c6d80ce6f7620956bc74
Fritz <fritz.developer10@gmail.com>
2023-12-12 22:48:07 -0500

    arreglos

---
+++ 02-intro-js/src/bases/04-arreglos.js
@@ -0,0 +1,16 @@
+// Arreglos en JS
+
+const arreglo = [1,2,3,4];
+
+let arreglo2 = [...arreglo,5];
+
+const arreglo3 = arreglo2.map(function(numero){
+    return numero * 2;
+});
+
+// el primer arreglo es un arreglo base
+console.log(arreglo);
+// el segundo arreglo  se genera con ... y se le agrega 5
+console.log(arreglo2);
+// el tercer arreglo se hace usando el etodo map (function(){return }), con el cual se ahce un nuevo arreglo agregandole una formula que uno desee.
+console.log(arreglo3);
